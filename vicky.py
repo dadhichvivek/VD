24974976ff0a2b0e918ebd0c081a0598655f92ed
prasanchandak <56474916+prasanchandak@users.noreply.github.com>
2019-10-15 23:44:41 +0530

    Update vicky.py

--- vicky.py
+++ vicky.py
@@ -1,6 +1,6 @@
-a=int(input("give me a number:"))
-b=int(input("give me a number:"))
-c=int(input("give me a number:"))
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
 n=(int(a)),(int(b)),(int(c))
 m=((int(a))**3+(int(b))**3+(int(c))**3)
 if (n==m):
